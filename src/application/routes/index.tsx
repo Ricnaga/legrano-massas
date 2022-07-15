9577fdc183ca09ee65c1ae62eb7b894e634b08e5
Ricnaga <ric.nagato@gmail.com>
2022-07-15 18:44:28 -0300

    feat(App): Configurações iniciais

---
+++ src/application/routes/index.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from '../../pages/Home/HomePage';
+import { MenuPage } from '../../pages/Menu/MenuPage';
+
+function AllRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/menu" element={<MenuPage />} />
+        </Routes>
+    )
+}
+
+type RouterProviderProps = {
+    children: ReactNode
+}
+
+export function RouterProvider({ children }: RouterProviderProps) {
+    return (
+        <BrowserRouter>
+            {children}
+            <AllRoutes />
+        </BrowserRouter>
+    )
+}
---
+++ src/pages/Menu/MenuPage.tsx
@@ -0,0 +1,3 @@
+export function MenuPage(){
+    return <h1>MenuPage</h1>
+}
