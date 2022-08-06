737aec24217538676f95e701485d24ceee84bf1e
Ricnaga <ric.nagato@gmail.com>
2022-08-06 18:08:11 -0300

    refactor: Ajuste de lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.ts?(x)': ['yarn typecheck', 'yarn lint:fix'],
+  '*.[j|t]s?(x)': ['yarn typecheck', 'yarn lint:fix'],
   '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
 };
