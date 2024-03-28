615f1b886d5c16754cbbd69053b1a73c15de523f
Ricardo Naga <ric.nagato@gmail.com>
2024-03-28 18:04:18 -0300

    feat(Container): Add Container component

---
+++ app/shared/components/Container/Container.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+import { styles } from './container.css';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export function Container(props: ContainerProps) {
+  return <div {...props} className={styles} />;
+}
---
+++ app/shared/components/Container/container.css.ts
@@ -0,0 +1,6 @@
+import { style } from '@vanilla-extract/css';
+
+export const styles = style({
+  padding: '2rem 2rem 0',
+  height: '100%',
+});
