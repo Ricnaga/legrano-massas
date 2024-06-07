a6092c0a5a17af92da7e01be9fd0ecfa6c44c2b8
Ricardo Naga <ric.nagato@gmail.com>
2024-06-07 18:11:47 -0300

    feat(theme): Add breakpoints

--- app/application/theme/theme.css.ts
+++ app/application/theme/theme.css.ts
@@ -20,3 +20,11 @@ export const colors = {
     return this.red.concat('42');
   },
 };
+
+export const breakpoints = {
+  sm: 'screen and (min-width: 640px)',
+  md: 'screen and (min-width: 768px)',
+  lg: 'screen and (min-width: 1024px)',
+  xl: 'screen and (min-width: 1280px)',
+  '2xl': 'screen and (min-width: 1536px)',
+};
