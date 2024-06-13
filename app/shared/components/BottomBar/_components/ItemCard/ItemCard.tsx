3de4c3349af8aee8e7ed4a83c7166d067280f618
Ricardo Naga <ric.nagato@gmail.com>
2024-06-13 17:00:40 -0300

    refactor(ItemsCard): Update component with Button and Card

--- app/shared/components/BottomBar/_components/ItemCard/ItemCard.tsx
+++ app/shared/components/BottomBar/_components/ItemCard/ItemCard.tsx
@@ -2,10 +2,10 @@
 
 import { useMenuContext } from '@/app/contexts/menu';
 import { MenuActionsType } from '@/app/contexts/menu/hooks/useMenuProvider';
-import { convertToBRL } from '@/app/shared/utils/currency';
-import { ProductCard } from '@/components';
-import { amountButton, amountContainer } from './itemcard.css';
 import { WhatsAppIcon } from '@/app/shared/icons';
+import { convertToBRL } from '@/app/shared/utils/currency';
+import { Button, Card, ProductCard, Typography } from '@/components';
+import { amountContainer, container } from './itemcard.css';
 
 export function ItemCard() {
   const { dispatch, state } = useMenuContext();
@@ -33,16 +33,17 @@ export function ItemCard() {
   );
 
   return (
-    <>
+    <div className={container()}>
       <ProductCard
         categories={selectedCategories}
-        itemsToScroll={8}
+        itemsToScroll={5}
         variant="Cart"
       >
         {(category) => (
           <div className={amountContainer()}>
-            <button
-              className={amountButton()}
+            <Button
+              radius="full"
+              isIconButton
               onClick={() =>
                 dispatch({
                   action: MenuActionsType.ADD_AMOUNT,
@@ -54,10 +55,11 @@ export function ItemCard() {
               }
             >
               +
-            </button>
+            </Button>
             <span>{category.amount}</span>
-            <button
-              className={amountButton()}
+            <Button
+              radius="full"
+              isIconButton
               onClick={() =>
                 dispatch({
                   action: MenuActionsType.REMOVE_FROM_CART,
@@ -69,14 +71,16 @@ export function ItemCard() {
               }
             >
               -
-            </button>
+            </Button>
           </div>
         )}
       </ProductCard>
       {!!selectedCategories.length && (
-        <>
-          <h2>Total: {totalPrice}</h2>
-          <button
+        <Card>
+          <Typography variant="h2" textAlign="right">
+            Total: {totalPrice}
+          </Typography>
+          <Button
             onClick={() =>
               dispatch({
                 action: MenuActionsType.CLEAR_CART,
@@ -87,12 +91,12 @@ export function ItemCard() {
             }
           >
             Limpar
-          </button>
-          <button>
+          </Button>
+          <Button>
             <WhatsAppIcon /> Me manda no zap
-          </button>
-        </>
+          </Button>
+        </Card>
       )}
-    </>
+    </div>
   );
 }
--- app/shared/components/BottomBar/_components/ItemCard/itemcard.css.ts
+++ app/shared/components/BottomBar/_components/ItemCard/itemcard.css.ts
@@ -1,5 +1,15 @@
-import { globalVars } from '@/app/application/theme/index.css';
 import { recipe } from '@vanilla-extract/recipes';
+
+export const container = recipe({
+  base: [
+    {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+    },
+  ],
+});
 
 export const amountContainer = recipe({
   base: {
@@ -7,58 +17,5 @@ export const amountContainer = recipe({
     gridTemplateColumns: 'repeat(3, 1fr)',
     alignItems: 'center',
     justifyItems: 'center',
-  },
-});
-
-export const amountButton = recipe({
-  base: {
-    display: 'flex',
-    alignItems: 'center',
-    gap: '1rem',
-
-    height: '100%',
-    maxHeight: '28px',
-
-    transition: 'all',
-    transitionDuration: '0.2s',
-
-    cursor: 'pointer',
-
-    background: globalVars.color.yellow200,
-
-    borderRadius: '28px',
-    border: 'none',
-
-    padding: '.8rem',
-
-    color: globalVars.color.white200,
-    fontSize: '1.5rem',
-    fontWeight: 700,
-
-    boxShadow: '0px 0px 4px 4px :color'.replace(
-      ':color',
-      globalVars.color.yellow100,
-    ),
-
-    selectors: {
-      '&:hover:not(:disabled)': {
-        background: globalVars.color.green200,
-
-        color: globalVars.color.yellow200,
-
-        boxShadow: '0px 0px 4px 4px :color'.replace(
-          ':color',
-          globalVars.color.green100,
-        ),
-      },
-      '&:active': {
-        filter: 'brightness(110%)',
-      },
-
-      '&:disabled': {
-        background: globalVars.color.green200,
-        cursor: 'not-allowed',
-      },
-    },
   },
 });
