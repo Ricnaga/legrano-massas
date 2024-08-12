557d48b7c49ba0e29f6aa165c4f8b9d80ef399a0
Ricardo Naga <ric.nagato@gmail.com>
2024-08-12 10:09:47 -0300

    refactor(components): Update barrel imports

--- app/shared/components/index.ts
+++ app/shared/components/index.ts
@@ -2,6 +2,5 @@ export * from './Button/Button';
 export * from './Card/Card';
 export * from './Container/Container';
 export * from './Drawer/Drawer';
-export * from './@legrano/ProductCard/ProductCard';
 export * from './Tabs/Tabs';
 export * from './Typography/Typography';
